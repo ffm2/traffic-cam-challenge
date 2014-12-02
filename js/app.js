d2d347588c9ced27792867f830738d5cd53894c6
Fardos Mohamed <ffm2@uw.edu>
2014-12-02 15:43:04 -0800

    all changes are finalized

--- js/app.js
+++ js/app.js
@@ -10,4 +10,58 @@
 //image
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
+$(document).ready(function() {
+    var mapElem = document.getElementById('map');
+    var center = {
+        lat: 47.6,
+        lng: -122.3
+    };
+    var map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 12
+    });
 
+    var infoWindow = new google.maps.InfoWindow();
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+        .done(function(data) {
+            data.forEach(function(camera) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: Number(camera.location.latitude),
+                        lng: Number(camera.location.longitude)
+                    },
+                    map: map
+                });
+                $("#search").bind('search keyup', function() {
+                    var d = $("#search").val();
+                    var label = camera.cameralabel;
+                    label = label.toLowerCase();
+                    d = d.toLowerCase();
+                    var n = label.indexOf(d);
+                    if(-1 == n)
+                        marker.setMap(null);
+                    else
+                        marker.setMap(map);
+                });
+                google.maps.event.addListener(marker, 'click', function() {
+                    var html = '<p>' + camera.cameralabel + '</p>';
+                    html += '<p><img src=' + camera.imageurl.url + '></p>';
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, this);
+                    var pos = this.getPosition();
+                    map.panTo(pos);
+                    marker.setAnimation(google.maps.Animation.DROP);
+                });
+                google.maps.event.addListener(map, 'click', function() {
+                    infoWindow.close();
+                });
+            });
+        })
+        .fail(function(error) {
+            alert("could not get files");
+        })
+        .always(function() {
+            $('#ajax-spinner').fadeOut();
+        });
+});
